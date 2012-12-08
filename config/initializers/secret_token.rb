76b42626a009459a4cb16551b8eb647091a81944
leemarshall <lpdmarshal@gmail.com>
2012-12-08 13:29:46 +0000

    growme first commit, home, sign up and logging in and out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Growme::Application.config.secret_token = 'cddfa0d82166af700bc9bf384a601d47d120e306f8098e7322e5926c162a93a45bd010a0ed1cad3ee3be76b59bea3cbce997b81c03eb63b162b2afb235850975'
